ce744b2f50a489ad023545bf8e79dc3ddfb0c352
Jose Luis Montes <jlmontesj@gmail.com>
2024-12-06 12:31:41 +0100

    updating libraries and versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.github.pepitoria.blinkoapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.github.pepitoria.blinkoapp"
         minSdk = 25
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
