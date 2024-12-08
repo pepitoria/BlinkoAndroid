5416babbda8517d8308298a15647355d6ca744a7
Jose Luis Montes Jiménez <jose.montes@edreamsodigeo.com>
2024-12-08 13:14:21 +0100

    loading indicator

--- app/src/main/java/com/github/pepitoria/blinkoapp/ui/login/TokenLoginScreenComposable.kt
+++ app/src/main/java/com/github/pepitoria/blinkoapp/ui/login/TokenLoginScreenComposable.kt
@@ -7,11 +7,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -49,17 +52,9 @@ fun TokenLoginWidget(
 
   BlinkoAppTheme {
     if (isLoading.value) {
-      Text(
-        text = "Loading...",
-        modifier = Modifier.fillMaxSize(),
-        color = Black
-      )
+      Loading()
     } else if (isSessionActive.value) {
-      Text(
-        text = "Session active",
-        modifier = Modifier.fillMaxSize(),
-        color = Black
-      )
+      SessionActive()
     } else {
       TokenLoginScreenViewState(
         url = viewModel.getStoredUrl() ?: "",
@@ -73,6 +68,32 @@ fun TokenLoginWidget(
       )
     }
   }
+}
+
+@Composable
+@Preview
+private fun Loading() {
+  Column(
+    modifier = Modifier.fillMaxSize(),
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+    CircularProgressIndicator(
+      modifier = Modifier.width(64.dp),
+      color = MaterialTheme.colorScheme.secondary,
+      trackColor = MaterialTheme.colorScheme.surfaceVariant,
+    )
+  }
+}
+
+@Composable
+@Preview
+private fun SessionActive() {
+  Text(
+    text = "Session active",
+    modifier = Modifier.fillMaxSize(),
+    color = Black
+  )
 }
 
 @Preview
