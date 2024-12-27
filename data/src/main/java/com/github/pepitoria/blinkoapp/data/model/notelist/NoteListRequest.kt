ce2d07ae3ed3af42516ac478e5f555e116fe4e99
Jose Luis Montes <jlmontesj@gmail.com>
2024-12-27 20:14:51 +0100

    adding fields to NoteListRequest

--- data/src/main/java/com/github/pepitoria/blinkoapp/data/model/notelist/NoteListRequest.kt
+++ data/src/main/java/com/github/pepitoria/blinkoapp/data/model/notelist/NoteListRequest.kt
@@ -9,10 +9,13 @@ data class NoteListRequest(
   @SerializedName("orderBy") val orderBy: String = "desc",
   @SerializedName("type") val type: Int = -1,
   @SerializedName("isArchived") val isArchived: Boolean = false,
+  @SerializedName("isShare") val isShare: String? = null,
   @SerializedName("isRecycle") val isRecycle: Boolean = false,
   @SerializedName("searchText") val searchText: String = "",
   @SerializedName("withoutTag") val withoutTag: Boolean = false,
   @SerializedName("withFile") val withFile: Boolean = false,
   @SerializedName("withLink") val withLink: Boolean = false,
-  @SerializedName("isUseAiQuery") val isUseAiQuery: Boolean = false
+  @SerializedName("isUseAiQuery") val isUseAiQuery: Boolean = false,
+  @SerializedName("startDate") val startDate: String? = null,
+  @SerializedName("endDate") val endDate: String? = null,
 )
