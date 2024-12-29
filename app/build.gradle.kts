ac5cde90d7a6d0d9dc6d0d28e12612a5bb02e95f
Jose Luis Montes <jlmontesj@gmail.com>
2024-12-29 20:05:40 +0100

    viewing notes with markdown lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,8 @@ dependencies {
   implementation(libs.androidx.hilt.navigation.compose)
   ksp(libs.hilt.compiler)
 
+  implementation(libs.richtext.commonmark)
+
   testImplementation(libs.junit)
   androidTestImplementation(libs.androidx.junit)
   androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/github/pepitoria/blinkoapp/ui/notelist/NoteListScreenComposable.kt
+++ app/src/main/java/com/github/pepitoria/blinkoapp/ui/notelist/NoteListScreenComposable.kt
@@ -1,15 +1,26 @@
 package com.github.pepitoria.blinkoapp.ui.notelist
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.github.pepitoria.blinkoapp.domain.model.note.BlinkoNote
 import com.github.pepitoria.blinkoapp.ui.base.ComposableLifecycleEvents
 import com.github.pepitoria.blinkoapp.ui.loading.Loading
 import com.github.pepitoria.blinkoapp.ui.theme.BlinkoAppTheme
+import com.halilibo.richtext.commonmark.Markdown
+import com.halilibo.richtext.ui.BasicRichText
 
 @Composable
 fun NoteListScreenComposable(
@@ -39,11 +50,36 @@ private fun EmptyNoteList() {
 @Composable
 private fun NoteList(notes: List<BlinkoNote>) {
 
-  Text("Notes list")
+  Column(
+    modifier = Modifier.padding(16.dp)
+  ) {
+    Text(
+      text = "Notes list",
+      fontSize = 24.sp
+    )
+    Spacer(modifier = Modifier.height(8.dp))
 
-  LazyColumn {
-    items(notes) { note ->
-      Text(note.content)
+    LazyColumn {
+      items(notes) { note ->
+        NoteListItem(note)
+        Spacer(modifier = Modifier.height(8.dp))
+      }
+    }
+  }
+}
+
+@Composable
+private fun NoteListItem(note: BlinkoNote) {
+
+  Card (
+    modifier = Modifier.fillMaxWidth()
+  ) {
+    BasicRichText(
+      modifier = Modifier.padding(16.dp)
+    ) {
+      Markdown(
+        content = note.content.trimIndent()
+      )
     }
   }
 }
