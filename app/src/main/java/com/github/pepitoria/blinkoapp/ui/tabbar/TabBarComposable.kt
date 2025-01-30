9e3bc952f8555dcc3a9a43fbf0924accd2c18013
Jose Luis Montes <jlmontesj@gmail.com>
2025-01-30 21:15:47 +0100

    icons for tab bar

--- app/src/main/java/com/github/pepitoria/blinkoapp/ui/tabbar/TabBarComposable.kt
+++ app/src/main/java/com/github/pepitoria/blinkoapp/ui/tabbar/TabBarComposable.kt
@@ -10,7 +10,11 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import com.github.pepitoria.blinkoapp.BlinkoNavigationRouter
+import com.github.pepitoria.blinkoapp.R
 
 @Composable
 fun TabBar(
@@ -23,8 +27,8 @@ fun TabBar(
     bottomBar = {
       NavigationBar {
         NavigationBarItem(
-          icon = { Icon(Icons.Default.Home, contentDescription = "Notes") },
-          label = { Text("Notes") },
+          icon = { Icon(ImageVector.vectorResource(id = R.drawable.note), contentDescription = stringResource(R.string.tab_bar_notes)) },
+          label = { Text(text = stringResource(R.string.tab_bar_notes)) },
           selected = currentRoute == BlinkoNavigationRouter.NavHome.NoteList.route,
           onClick = {
             if (currentRoute != BlinkoNavigationRouter.NavHome.NoteList.route) {
@@ -34,8 +38,8 @@ fun TabBar(
         )
 
         NavigationBarItem(
-          icon = { Icon(Icons.Default.Search, contentDescription = "Blinkos") },
-          label = { Text("Blinkos") },
+          icon = { Icon(ImageVector.vectorResource(id = R.drawable.blinko), contentDescription = stringResource(R.string.tab_bar_blinkos)) },
+          label = { Text(text = stringResource(R.string.tab_bar_blinkos)) },
           selected = currentRoute == BlinkoNavigationRouter.NavHome.BlinkoList.route,
           onClick = {
             if (currentRoute != BlinkoNavigationRouter.NavHome.BlinkoList.route) {
