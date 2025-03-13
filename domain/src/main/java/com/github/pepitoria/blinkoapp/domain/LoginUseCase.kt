a787ed8bc09d74af266e15c9eebe15448e2889f1
Jose Luis Montes <jlmontesj@gmail.com>
2025-03-13 08:11:41 +0100

    deprecated

--- domain/src/main/java/com/github/pepitoria/blinkoapp/domain/LoginUseCase.kt
+++ domain/src/main/java/com/github/pepitoria/blinkoapp/domain/LoginUseCase.kt
@@ -4,6 +4,7 @@ import com.github.pepitoria.blinkoapp.data.model.login.LoginRequest
 import com.github.pepitoria.blinkoapp.data.repository.auth.AuthenticationRepository
 import javax.inject.Inject
 
+@Deprecated("Use TokenLoginWidget instead")
 class LoginUseCase @Inject constructor(
     private val authenticationRepository: AuthenticationRepository
 ) {
