2eeac60e4c066d4a7740e7d7d50141e17b3ae9a7
José Luis Montes Jiménez <jlmontesj@gmail.com>
2025-04-25 13:20:10 +0200

    updating version code and version name 0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.github.pepitoria.blinkoapp"
     minSdk = libs.versions.minSdk.get().toInt()
     targetSdk = libs.versions.targetSdk.get().toInt()
-    versionCode = 1
-    versionName = "0.1"
+    versionCode = 3
+    versionName = "0.2.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables {
