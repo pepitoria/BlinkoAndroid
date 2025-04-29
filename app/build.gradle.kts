f1c51ef559228aecdaedff4cd921bd19bab5299b
Jose Luis Montes <jlmontesj@gmail.com>
2025-04-29 08:24:38 +0200

    adding scripts so I can create releases in github from the cli

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,15 @@
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
+import okhttp3.OkHttpClient
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
 import java.io.FileInputStream
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 import java.util.Properties
+import java.util.zip.CRC32
 
 plugins {
   alias(libs.plugins.androidApplication)
@@ -7,6 +17,12 @@ plugins {
   alias(libs.plugins.kotlin.compose.compiler)
   alias(libs.plugins.hilt.android)
   alias(libs.plugins.ksp)
+}
+
+buildscript {
+  dependencies {
+    classpath(libs.okhttp3.okhttp)
+  }
 }
 
 android {
@@ -97,21 +113,25 @@ tasks.register("newRelease") {
 
   doLast {
     incrementCodeVersion()
-    val versionCode = getCodeVersion()
-    val versionName = getVersionNameFromGit()
 
     val apkFile = file("build/outputs/apk/remote/release/app-remote-release.apk")
     val destinationDir = file("../")
 
     if (apkFile.exists()) {
       destinationDir.mkdirs()
-      val renamedApk = File(destinationDir, "BlinkoApp-$versionName-($versionCode).apk")
+      val renamedApk = File(destinationDir, getApkName())
       apkFile.copyTo(renamedApk, overwrite = true)
       println("APK copied to: ${renamedApk.absolutePath}")
     } else {
       println("release APK NOT FOUND.")
     }
   }
+}
+
+fun getApkName(): String {
+  val versionCodeHash = getShortHashedVersionCode()
+  val versionName = getVersionNameFromGit()
+  return "BlinkoApp-$versionName-$versionCodeHash.apk"
 }
 
 fun getVersionNameFromGit(): String {
@@ -145,10 +165,94 @@ fun incrementCodeVersion() {
   }
 
   val code = versionProps["VERSION_CODE"].toString().toInt() + 1
-
   versionProps["VERSION_CODE"] = code.toString()
   versionProps.store(versionPropsFile.writer(), null)
 }
+
+fun getShortHashedVersionCode(): String {
+  val versionCode = getCodeVersion().toString()
+  val crc = CRC32()
+  crc.update(versionCode.toByteArray())
+  return crc.value.toString(16)
+}
+
+fun getReleaseDescription(): String {
+  val lastTagCommand = Runtime.getRuntime().exec("git describe --tags --abbrev=0")
+  val lastTag = lastTagCommand.inputStream.bufferedReader().readText().trim()
+
+  val prevTagCommand = Runtime.getRuntime().exec("git describe --tags --abbrev=0 \$(git rev-list --tags --skip=1 --max-count=1)")
+  val prevTag = prevTagCommand.inputStream.bufferedReader().readText().trim()
+
+  val commitsBetweenTagsCommand = Runtime.getRuntime().exec("git log $prevTag..$lastTag --oneline")
+  val commitsBetweenTags = commitsBetweenTagsCommand.inputStream.bufferedReader().readText().trim()
+
+  return commitsBetweenTags
+}
+
+fun getGithubToken(): String {
+  val properties = Properties().apply {
+    load(File("../blinkoapp.github.properties").reader())
+  }
+  return properties["GITHUB_TOKEN"].toString().trim()
+}
+
+tasks.register("uploadApkToGitHub") {
+  doLast {
+    val githubToken = getGithubToken()
+    val repoOwner = "pepitoria"
+    val repoName = "BlinkoAndroid"
+    val tagName = getVersionNameFromGit()
+    val releaseName = "Release $tagName"
+    val releaseDescription = getReleaseDescription()
+    val apkFilePath = "../${getApkName()}"
+
+    val apkFile = file(apkFilePath)
+    if (!apkFile.exists()) {
+      throw GradleException("APK not found: $apkFilePath")
+    }
+
+    val createReleaseUrl = "https://api.github.com/repos/$repoOwner/$repoName/releases"
+    val releaseBody = JsonObject().apply {
+      addProperty("tag_name", tagName)
+      addProperty("name", releaseName)
+      addProperty("body", releaseDescription)
+      addProperty("draft", false)
+      addProperty("prerelease", false)
+    }
+
+    val createReleaseRequest = Request.Builder()
+      .url(createReleaseUrl)
+      .addHeader("Authorization", "token $githubToken")
+      .addHeader("Content-Type", "application/json")
+      .post(releaseBody.toString().toRequestBody("application/json".toMediaType()))
+      .build()
+
+    val client = OkHttpClient()
+    val createReleaseResponse = client.newCall(createReleaseRequest).execute()
+    if (!createReleaseResponse.isSuccessful) {
+      throw GradleException("Error creating the release: ${createReleaseResponse.body?.string()}")
+    }
+
+    val releaseResponseJson = JsonParser.parseString(createReleaseResponse.body?.string()).asJsonObject
+    val encodedFileName = URLEncoder.encode(apkFile.name, StandardCharsets.UTF_8.toString())
+    val uploadUrl =   releaseResponseJson["upload_url"].asString.replace("{?name,label}", "?name=$encodedFileName")
+
+    val uploadApkRequest = Request.Builder()
+      .url(uploadUrl)
+      .addHeader("Authorization", "token $githubToken")
+      .addHeader("Content-Type", "application/vnd.android.package-archive")
+      .post(apkFile.asRequestBody("application/vnd.android.package-archive".toMediaType()))
+      .build()
+
+    val uploadApkResponse = client.newCall(uploadApkRequest).execute()
+    if (!uploadApkResponse.isSuccessful) {
+      throw GradleException("Error uploading APK: ${uploadApkResponse.body?.string()}")
+    }
+
+    println("APK uploaded successfully to the release in GitHub.")
+  }
+}
+
 
 dependencies {
   implementation(project(":domain"))
---
+++ newReleaseInGithub.sh
@@ -0,0 +1,6 @@
+# 1. create a new release tag
+# 2. commit and push everything
+# 3. launch:
+
+./gradlew newRelease
+./gradlew uploadApkToGitHub
