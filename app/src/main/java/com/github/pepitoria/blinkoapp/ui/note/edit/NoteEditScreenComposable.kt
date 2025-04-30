4be09f3755cb5bc91db3ab76d55c83d018394f3f
Jose Luis Montes <jlmontesj@gmail.com>
2025-04-30 08:15:55 +0200

    fixing cancel/save buttons not showing on very long/full screen notes.

--- app/src/main/java/com/github/pepitoria/blinkoapp/ui/note/edit/NoteEditScreenComposable.kt
+++ app/src/main/java/com/github/pepitoria/blinkoapp/ui/note/edit/NoteEditScreenComposable.kt
@@ -1,6 +1,6 @@
 package com.github.pepitoria.blinkoapp.ui.note.edit
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -9,6 +9,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -18,6 +19,7 @@ import com.github.pepitoria.blinkoapp.R
 import com.github.pepitoria.blinkoapp.domain.model.note.BlinkoNote
 import com.github.pepitoria.blinkoapp.ui.base.ComposableLifecycleEvents
 import com.github.pepitoria.blinkoapp.ui.loading.Loading
+import com.github.pepitoria.blinkoapp.ui.theme.BlinkoAppTheme
 
 @Composable
 fun NoteEditScreenComposable(
@@ -32,16 +34,18 @@ fun NoteEditScreenComposable(
 
   viewModel.onStart(noteId = noteId, onNoteUpsert = goBack)
 
-  if (isLoading.value) {
-    Loading()
-  } else {
-    BlinkoNoteEditor(
-      uiState = uiState.value,
-      modifier = Modifier.fillMaxWidth(),
-      updateNote = { viewModel.updateLocalNote(it.content) },
-      sendToBlinko = { viewModel.editNote() },
-      goBack = goBack,
-    )
+  BlinkoAppTheme {
+    if (isLoading.value) {
+      Loading()
+    } else {
+      BlinkoNoteEditor(
+        uiState = uiState.value,
+        modifier = Modifier.fillMaxWidth(),
+        updateNote = { viewModel.updateLocalNote(it.content) },
+        sendToBlinko = { viewModel.editNote() },
+        goBack = { goBack() },
+      )
+    }
   }
 }
 
@@ -53,7 +57,7 @@ fun BlinkoNoteEditor(
   sendToBlinko: () -> Unit = {},
   goBack: () -> Unit = {},
 ) {
-  Column(
+  Box(
     modifier = modifier
       .fillMaxWidth()
       .padding(16.dp)
@@ -63,11 +67,16 @@ fun BlinkoNoteEditor(
       onValueChange = { updateNote(uiState.copy(content = it)) },
       label = { Text(text = stringResource(R.string.note_edit_label)) },
       minLines = 3,
-      modifier = Modifier.fillMaxWidth()
+      modifier = Modifier
+        .fillMaxWidth()
+        .align(Alignment.TopStart)
+        .padding(bottom = 64.dp)
     )
 
     Row(
-      modifier = Modifier.fillMaxWidth()
+      modifier = Modifier
+        .fillMaxWidth()
+        .align(Alignment.BottomCenter)
     ) {
       CancelButton(
         modifier = Modifier.weight(1f),
