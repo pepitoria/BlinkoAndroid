f7c995d6c6025c5fac4eda3cffa8b41231e24c68
Jose Luis Montes <jlmontesj@gmail.com>
2025-06-07 20:33:42 +0200

    WIP: hiding tag list in search for now

--- feature-search/implementation/src/main/java/com/github/pepitoria/blinkoapp/search/implementation/SearchScreenComposable.kt
+++ feature-search/implementation/src/main/java/com/github/pepitoria/blinkoapp/search/implementation/SearchScreenComposable.kt
@@ -178,7 +178,7 @@ private fun EmptySearch(
     color = Color.White,
   )
 
-  tagsFactory.TagListComposable()
+  //tagsFactory.TagListComposable()
 }
 
 private fun getTagsFactory(context: Context, isInPreview: Boolean): TagsFactory {
