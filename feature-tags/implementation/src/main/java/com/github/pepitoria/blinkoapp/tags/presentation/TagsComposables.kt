812875d2f3b5ee98df74a1f3de9e5526902b0204
Jose Luis Montes <jlmontesj@gmail.com>
2025-06-08 20:15:38 +0200

    Removed unused imports from TagsComposables.kt

--- feature-tags/implementation/src/main/java/com/github/pepitoria/blinkoapp/tags/presentation/TagsComposables.kt
+++ feature-tags/implementation/src/main/java/com/github/pepitoria/blinkoapp/tags/presentation/TagsComposables.kt
@@ -6,9 +6,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
