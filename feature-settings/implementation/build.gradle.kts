7fa618329eb89e3c439dd0d707873e517475c107
Jose Luis Montes <jlmontesj@gmail.com>
2026-02-15 14:24:47 +0100

    tests in feature-settings

--- feature-settings/implementation/build.gradle.kts
+++ feature-settings/implementation/build.gradle.kts
@@ -54,6 +54,12 @@ android {
     compose = true
     buildConfig = true
   }
+
+  testOptions {
+    unitTests.all {
+      it.useJUnitPlatform()
+    }
+  }
 }
 
 dependencies {
@@ -96,6 +102,9 @@ dependencies {
 
   // testing
   testImplementation(libs.junit.jupiter)
+  testImplementation(libs.mockk)
+  testImplementation(libs.kotlinx.coroutines.test)
+  testImplementation(kotlin("test"))
   androidTestImplementation(libs.androidx.junit)
   androidTestImplementation(libs.androidx.espresso.core)
 }
